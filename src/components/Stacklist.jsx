db7ae2c8010bba60a66c79d5de55277b7cf4fd28
Kyle Treman <jkt290@nyu.edu>
2018-05-12 23:18:44 -0400

    Connecting redux to component

--- src/components/Stacklist.jsx
+++ src/components/Stacklist.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import stacks from "../data/stacks.json";
 import { Link } from "react-router-dom";
+import { setStack } from '../actions';
 
-export default class Stacklist extends Component {
+class Stacklist extends Component {
   render() {
     return (
       <div>
@@ -17,3 +20,16 @@ export default class Stacklist extends Component {
     );
   }
 }
+// redux will autonmatically recognize this function
+const mapDispatchToProps = (dispatch) => {
+  // add action creators you want to bind
+  return bindActionCreators({ setStack }, dispatch);
+}
+
+// takes two parameters, both are functions
+// first handles connecting the component ot the store and retreving data
+// second allows components to use the action creators and use reducers to update store
+// returns a function that connects component
+const connectComponent = connect(null, mapDispatchToProps);
+
+export default connectComponent(Stacklist);
