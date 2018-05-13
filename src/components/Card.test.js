332e8590ab76e95ab669102105048c787bda9d57
Kyle Treman <jkt290@nyu.edu>
2018-05-13 19:06:25 -0400

    Add answer render test

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -3,16 +3,20 @@ import { shallow } from "enzyme";
 import Card from "./Card";
 import "../setupTests";
 
-const props = { card: { prompt: 'test prompt', answer: 'test answer'}}
+const props = { card: { prompt: "test prompt", answer: "test answer" } };
 
 describe("Card", () => {
-  const card = shallow(<Card { ...props } />);
+  const card = shallow(<Card {...props} />);
 
-  it('sets `reveal` to be `false`', () => {
+  it("sets `reveal` to be `false`", () => {
     expect(card.state().reveal).toBe(false);
-  })
+  });
 
   it("renders the card prompt", () => {
-    expect(card.find('.card-prompt h4').text()).toEqual(props.card.prompt)
-  })
-})
+    expect(card.find(".card-prompt h4").text()).toEqual(props.card.prompt);
+  });
+
+  it("renders the card answer", () => {
+    expect(card.find(".card-answer h4").text()).toEqual(props.card.answer);
+  });
+});
