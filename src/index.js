860b01b1bc892535bd4a8aad056f078e91b20357
Kyle Treman <jkt290@nyu.edu>
2018-05-12 23:09:10 -0400

    Add Provider wrapper

--- src/index.js
+++ src/index.js
@@ -1,28 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from 'redux';
+import { createStore } from "redux";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Provider } from "react-redux";
 // importing stack because its default, just calling it rootReducer for convention
-import rootReducer from './reducers';
+import rootReducer from "./reducers";
 import App from "./components/App";
 import Stack from "./components/Stack";
-import { setStack } from './actions';
-
+import { setStack } from "./actions";
 
 const store = createStore(rootReducer);
 // fire off code when store updates
 // code with getState of the store
 store.subscribe(() => {
-  console.log('store', store.getState())
+  console.log("store", store.getState());
 });
-store.dispatch(setStack({ id: 0, title: 'example', cards: [] }));
+store.dispatch(setStack({ id: 0, title: "example", cards: [] }));
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route path="/stack" component={Stack} />
-    </Switch>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/stack" component={Stack} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
