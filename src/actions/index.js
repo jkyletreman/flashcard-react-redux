4ee5c73e7e56ea7419b2c2f77664deda75b80aa2
Kyle Treman <jkt290@nyu.edu>
2018-05-12 22:57:51 -0400

    Change type to variable

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,9 @@
+const SET_STACK = 'SET_STACK'
 // function is a action creator, which returns the action
 const setStack = (stack) => {
   return {
     // all redux actions have type, use scream stack convention
-    type: 'SET_STACK',
+    type: SET_STACK,
     // then the action has data, which is what the actions acts on
     // will come from any component that can set the stack (each component needs the ability to set the data)
     // wrapping it in a function, allows a paramater to be created for stack data
@@ -10,4 +11,4 @@ const setStack = (stack) => {
   };
 }
 
-export { setStack }; 
+export { setStack, SET_STACK };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,10 @@
+import { SET_STACK } from '../actions'
 // reducer has 1 primary goal, compose a object to represent how the state should be updated
 // state uses the object to represent the data in the reducer
 // each reducer takes two paramaters the first is the redux state at a current time, the second is the action
 const stack = (state = {}, action) => {
   switch (action.type) {
-    case "SET_STACK":
+    case SET_STACK:
       return action.stack;
     default:
       return state;
