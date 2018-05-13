fbf65365c4b79d848f5ca909e38a5d9dfd61c3c5
Kyle Treman <jkt290@nyu.edu>
2018-05-12 23:53:51 -0400

    Fix html

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import Card from "./Card";
 
 class Stack extends Component {
@@ -8,24 +8,22 @@ class Stack extends Component {
     const { title, cards } = this.props.stack;
     return (
       <div>
-        <Link to='/' className="link-home">Home</Link>
-          <h3>{title}</h3>
-          <br />
-          {
-            cards.map(card => {
-              return (
-                <Card key={card.id} card={card}/>
-              )
-            })
-          }
+        <Link to="/" className="link-home">
+          <h4>Home</h4>
+        </Link>
+        <h3>{title}</h3>
+        <br />
+        {cards.map(card => {
+          return <Card key={card.id} card={card} />;
+        })}
       </div>
-    )
+    );
   }
 }
 // takes state as parameter
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   // want to return a object that we want to be the props
-  return { stack: state }
-}
+  return { stack: state };
+};
 
 export default connect(mapStateToProps, null)(Stack);
