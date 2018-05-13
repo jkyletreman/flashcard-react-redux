611684ed37786bbb827a6924afc1f68010efaed6
Kyle Treman <jkt290@nyu.edu>
2018-05-12 22:40:51 -0400

    add route to stack

--- src/components/Stacklist.jsx
+++ src/components/Stacklist.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
 import stacks from "../data/stacks.json";
+import { Link } from 'react-router-dom';
 
 export default class Stacklist extends Component {
   render() {
     return (
       <div>
         {stacks.map(stack => {
-          return <h4 key={stack.id}>{stack.title}</h4>;
-        })}
+          return (
+            <Link to="/stack">
+              <h4 key={stack.id}>{stack.title}</h4>;
+            </Link>
+        )})}
       </div>
     );
   }
