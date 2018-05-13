6eb801e7cae9a39e16e2cb300897c5d7953cc512
Kyle Treman <jkt290@nyu.edu>
2018-05-13 18:36:11 -0400

    Add chnage title test

--- src/components/StackForm.test.js
+++ src/components/StackForm.test.js
@@ -25,5 +25,16 @@ describe("StackForm", () => {
 
   it("renders a button to submit the form", () => {
     expect(stackForm.find('Button').at(1).props().children).toEqual("Save and Add the Stack")
+  });
+
+  describe('and updating the title', () => {
+    beforeEach(() => {
+      // need to simulate the target.value from our onChange in the form
+      stackForm.find('FormControl').simulate('change', { target: { value: 'change title' }})
+    })
+
+    it("updates the title in the state", () => {
+      expect(stackForm.state().title).toEqual('change title')
+    })
   })
 })
