09280a3ee3df14dd967a1828b3c9ab6697f9fea1
Kyle Treman <jkt290@nyu.edu>
2018-05-13 00:23:10 -0400

    Add StackForm

---
+++ src/components/StackForm.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class StackForm extends Component {
+  render() {
+    <div>
+      <h4>Create a New Stack</h4>
+    </div>
+  }
+}
+
+export default StackForm;
