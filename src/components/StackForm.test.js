accae2001c258c19058299a4b1f5628b05afa719
Kyle Treman <jkt290@nyu.edu>
2018-05-13 18:41:27 -0400

    Add form behavior tests

--- src/components/StackForm.test.js
+++ src/components/StackForm.test.js
@@ -36,5 +36,23 @@ describe("StackForm", () => {
     it("updates the title in the state", () => {
       expect(stackForm.state().title).toEqual('change title')
     })
+  });
+
+  describe("when adding a new card", () => {
+    beforeEach(() => {
+      stackForm.find("Button").at(0).simulate('click');
+    });
+
+    it("adds a new card to the state", () => {
+      expect(stackForm.state().cards.length).toEqual(1);
+    });
+
+    it("renders the prompt section", () => {
+      expect(stackForm.find("ControlLabel").at(1).props().children).toEqual("Prompt:")
+    })
+
+    it("renders the answer section", () => {
+      expect(stackForm.find("ControlLabel").at(2).props().children).toEqual("Answer:")
+    })
   })
 })
