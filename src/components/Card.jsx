f11064d7d2ad4fda78eefc8a386e4db1f1466d7e
Kyle Treman <jkt290@nyu.edu>
2018-05-13 00:05:03 -0400

    Add answer reveal

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Card extends Component {
-  render () {
+  constructor() {
+    super();
+    this.state = { reveal: false };
+  }
+  render() {
     const { prompt, answer } = this.props.card;
 
     return (
-      <div className='card'>
-        <div className='card-prompt'><h4>{prompt}</h4></div>
-        <div className='card-answer'><h4>{answer}</h4></div>
+      <div
+        className="card"
+        onClick={() => {
+          this.setState({ reveal: true });
+        }}
+      >
+        <div className="card-prompt">
+          <h4>{prompt}</h4>
+        </div>
+        <div className="card-answer">
+          <h4 className={this.state.reveal ? "text-revealed" : "text-hidden"}>
+            {answer}
+          </h4>
+        </div>
       </div>
-    )
+    );
   }
 }
 
