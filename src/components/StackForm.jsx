e9b6926ee2519a880f8ab6cff3466bbdd80797db
Kyle Treman <jkt290@nyu.edu>
2018-05-13 00:39:14 -0400

    Add more to form

--- src/components/StackForm.jsx
+++ src/components/StackForm.jsx
@@ -1,12 +1,38 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
+import { Form, FormGroup, FormControl, ControlLabel, Button } from 'react-bootstrap';
 
 class StackForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+      cards: []
+    }
+  }
+
+  addCard = () => {
+    const { cards } = this.state;
+
+    cards.push({ id: cards.length, prompt: '', answer: '' });
+
+    this.setState({ cards });
+  }
   render() {
     return (
       <div>
         <Link to='/' className='link-home'><h3>Home</h3></Link>
         <h3>Create a New Stack</h3>
+        <br />
+        <Form inline>
+          <FormGroup>
+            <ControlLabel>Title:</ControlLabel>
+            {' '}
+            <FormControl />
+          </FormGroup>
+        </Form>
+        <br />
+        <Button onClick={() => this.addCard()}>Add Card</Button>
       </div>
     );
   }
