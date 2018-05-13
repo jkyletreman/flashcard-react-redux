5de3bc62f09f98f6d5b4b9673064cf3e53da797f
Kyle Treman <jkt290@nyu.edu>
2018-05-12 22:39:31 -0400

    Add route home

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Stack extends Component {
   render() {
     return (
       <div>
-        <h3>example title</h3>
+        <Link to='/'>Home</Link>
+          <h3>example title</h3>
       </div>
     )
   }
