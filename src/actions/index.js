d36bb678eff0720d95b738978876c694b371bb48
Kyle Treman <jkt290@nyu.edu>
2018-05-12 22:49:07 -0400

    Add action that sets the stack

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+// function is a action creator, which returns the action
+const setStack = (stack) => {
+  return {
+    // all redux actions have type, use scream stack convention
+    type: 'SET_STACK',
+    // then the action has data, which is what the actions acts on
+    // will come from any component that can set the stack (each component needs the ability to set the data)
+    // wrapping it in a function, allows a paramater to be created for stack data
+    stack: stack
+  };
+}
+
+export { setStack }; 
