c9e2b3615eaca4c1a7864b73dca1bd8681ec1dfb
Kyle Treman <jkt290@nyu.edu>
2018-05-12 23:01:36 -0400

    Add redux store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from 'redux';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+// importing stack because its default, just calling it rootReducer for convention
+import rootReducer from './reducers';
 import App from "./components/App";
 import Stack from "./components/Stack";
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <BrowserRouter>
