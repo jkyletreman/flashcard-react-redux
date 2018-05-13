23cf5ba8004ffa69e577271a1f37d03284014941
Kyle Treman <jkt290@nyu.edu>
2018-05-13 18:31:01 -0400

    Add unit tests

--- src/components/StackForm.test.js
+++ src/components/StackForm.test.js
@@ -1,5 +1,29 @@
 import React from "react";
 import { shallow } from "enzyme";
-import { Stacklist } from "./Stacklist";
+import { StackForm } from "./StackForm";
 import "../setupTests";
 import { stacks } from '../data/fixtures'
+
+describe("StackForm", () => {
+  const stackForm = shallow(<StackForm />);
+
+  it("renders the form title", () => {
+    expect(stackForm.find('h3').at(1).text()).toEqual("Create a New Stack");
+  });
+
+  it("renders a link home", () => {
+    expect(stackForm.find('h3').at(0).text()).toEqual("Home");
+  });
+
+  it("renders a Form component", () => {
+    expect(stackForm.find('Form').exists()).toBe(true);
+  });
+
+  it("renders a button to add a new card", () => {
+    expect(stackForm.find('Button').at(0).props().children).toEqual('Add Card');
+  });
+
+  it("renders a button to submit the form", () => {
+    expect(stackForm.find('Button').at(1).props().children).toEqual("Save and Add the Stack")
+  })
+})
