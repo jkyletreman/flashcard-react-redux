b2bee8962ab59d72c8b15b9d7d9d5d423c86aa7e
Kyle Treman <jkt290@nyu.edu>
2018-05-12 22:37:31 -0400

    boilerplate

---
+++ src/components/Stack.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Stack extends Component {
+  render() {
+    return (
+      <div>
+        <h3>example title</h3>
+      </div>
+    )
+  }
+}
