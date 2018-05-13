46bcc9d51000fc9c97f35fbccae46651fa11d1da
Kyle Treman <jkt290@nyu.edu>
2018-05-13 00:29:08 -0400

    Add Link home

--- src/components/StackForm.jsx
+++ src/components/StackForm.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom';
 
 class StackForm extends Component {
   render() {
     return (
       <div>
-        <h4>Create a New Stack</h4>
+        <Link to='/' className='link-home'><h3>Home</h3></Link>
+        <h3>Create a New Stack</h3>
       </div>
     );
   }
