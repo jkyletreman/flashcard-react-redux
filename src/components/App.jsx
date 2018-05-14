152442a3d90c7ac26e17b29a61eab0ad00abda07
Kyle Treman <jkt290@nyu.edu>
2018-05-13 20:42:00 -0400

    convert to functional component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,19 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
-import Stacklist from './Stacklist';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Stacklist from "./Stacklist";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Flashcard Pro</h2>
-        <hr />
-        <Stacklist />
-        <hr />
-        <Link to='stack_form'><h4>Create a New Stack</h4></Link>
-      </div>
-    )
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <h2>Flashcard Pro</h2>
+      <hr />
+      <Stacklist />
+      <hr />
+      <Link to="stack_form">
+        <h4>Create a New Stack</h4>
+      </Link>
+    </div>
+  );
+};
+
+export default App;
