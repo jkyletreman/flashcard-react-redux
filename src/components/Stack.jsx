4b6e349bf80e7ee1fd1c0cebdb2f4f0c1fcd1b8c
Kyle Treman <jkt290@nyu.edu>
2018-05-13 20:46:00 -0400

    Convert to functional component

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -3,23 +3,21 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import Card from "./Card";
 
-export class Stack extends Component {
-  render() {
-    const { title, cards } = this.props.stack;
-    return (
-      <div>
-        <Link to="/" className="link-home">
-          <h3>Home</h3>
-        </Link>
-        <h3>{title}</h3>
-        <br />
-        {cards.map(card => {
-          return <Card key={card.id} card={card} />;
-        })}
-      </div>
-    );
-  }
-}
+export const Stack = ({ stack: { title, cards } }) => {
+  return (
+    <div>
+      <Link to="/" className="link-home">
+        <h3>Home</h3>
+      </Link>
+      <h3>{title}</h3>
+      <br />
+      {cards.map(card => {
+        return <Card key={card.id} card={card} />;
+      })}
+    </div>
+  );
+};
+
 // takes state as parameter
 const mapStateToProps = state => {
   // want to return a object that we want to be the props
